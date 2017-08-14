7465ae1310414ad1a6a54ff955e2f5bedef2a697
Marco Fugaro <marco.fugaro@gmail.com>
2017-08-14 22:51:08 +0200

    🎁  Separate compressed and uncompressed bundle

--- boilerplate/rollup.config.js
+++ boilerplate/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import bundleSize from 'rollup-plugin-bundle-size'
 import pkg from './package.json'
 
 export default {
@@ -14,7 +13,6 @@ export default {
     babel({
       exclude: 'node_modules/**',
     }),
-    bundleSize(),
   ],
   dest: pkg.main,
 }
